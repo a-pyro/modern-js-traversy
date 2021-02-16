161103620bfbafa972b83ef5b4f6a21686651087
ardigermenji <ardi.germenji@gmail.com>
2021-02-16 15:11:15 +0100

    d

--- 03-async-js-api/7_15_cust-http-lib-fetch-async-await/ardi/app.js
+++ 03-async-js-api/7_15_cust-http-lib-fetch-async-await/ardi/app.js
@@ -1,10 +1,11 @@
-const getEndpoint = 'https://jsonplaceholder.typicode.com/users';
+const getEndpoint =
+  'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy';
 const postEndpoint = 'https://jsonplaceholder.typicode.com/posts';
 const putEndpoint = 'https://jsonplaceholder.typicode.com/posts/1';
 const deleteEndpoint = 'https://jsonplaceholder.typicode.com/posts/1';
 
-get(getEndpoint).then((users) => console.table(users));
+get(getEndpoint).then((users) => console.log(users));
 
-fetch(getEndpoint)
-  .then((res) => res.json())
-  .then((data) => console.table(data));
+// fetch(getEndpoint)
+//   .then((res) => res.json())
+//   .then((data) => console.table(data));
